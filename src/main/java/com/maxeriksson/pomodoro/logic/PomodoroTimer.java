4e2f07509d59d4532e99c265adec5b4ac161b5c6
Max <teratoscincus@hotmail.com>
2024-06-11 10:31:02 +0200

    feat(logic): add `javax.swing.Timer` component

--- src/main/java/com/maxeriksson/pomodoro/logic/PomodoroTimer.java
+++ src/main/java/com/maxeriksson/pomodoro/logic/PomodoroTimer.java
@@ -1,14 +1,40 @@
 package com.maxeriksson.pomodoro.logic;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.Timer;
+
 /** PomodoroTimer */
 public class PomodoroTimer {
 
+    private final Timer TIMER;
+
     private PomodoroState state;
+    private int startSeconds;
     private int secondsLeft;
 
     public PomodoroTimer() {
         state = PomodoroState.FOCUS;
-        secondsLeft = 25 * 60;
+        startSeconds = 25 * 60;
+        secondsLeft = startSeconds;
+
+        final int SECOND_IN_MILLISECONDS = 1000;
+        TIMER =
+                new Timer(
+                        SECOND_IN_MILLISECONDS,
+                        new ActionListener() {
+                            @Override
+                            public void actionPerformed(ActionEvent e) {
+                                secondsLeft -= 1;
+                            }
+                        });
+    }
+
+    public void start() {
+        if (!TIMER.isRunning()) {
+            TIMER.start();
+        }
     }
 
     public String getState() {
