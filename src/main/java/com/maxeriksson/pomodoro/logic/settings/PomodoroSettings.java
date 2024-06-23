96f09a9195d7552491bd3e1f1184b5b74d461e86
Max <teratoscincus@hotmail.com>
2024-06-23 12:51:02 +0200

    chore(logic): add javadoc and `.MIN_FOCUS_MINUTES`
    `public static final int MIN_FOCUS_MINUTES` to replace magic number.

--- src/main/java/com/maxeriksson/pomodoro/logic/settings/PomodoroSettings.java
+++ src/main/java/com/maxeriksson/pomodoro/logic/settings/PomodoroSettings.java
@@ -3,17 +3,34 @@ package com.maxeriksson.pomodoro.logic.settings;
 /** PomodoroSettings */
 public class PomodoroSettings {
 
+    /**
+     * The minimum required length of the focus interval to ensure the break intervals are greater
+     * than 0 minutes.
+     */
+    public static final int MIN_FOCUS_MINUTES = 5;
+
     private int focusMinutes;
     private int shortBreakMinutes;
     private int longBreakMinutes;
 
     public PomodoroSettings() {
-        int defaultFocusMinutes = 25;
+        int defaultFocusMinutes = 25; // As per the Pomodoro technique
         setFocusMinutes(defaultFocusMinutes);
     }
 
+    /**
+     * Sets length of all other time intervals, in relation to given argument, as per the Pomodoro
+     * technique:
+     *
+     * <ul>
+     *   <li>The short break is a fifth the length of the focus interval. <br>
+     *   <li>The long break is tree times the length of the short break interval.
+     *
+     * @param focusMinutes min accepted value: 5 <br>
+     *     This is to ensure the break intervals are greater than 0.
+     */
     public void setFocusMinutes(int focusMinutes) {
-        if (focusMinutes >= 5) {
+        if (focusMinutes >= MIN_FOCUS_MINUTES) {
             this.focusMinutes = focusMinutes;
             shortBreakMinutes = focusMinutes / 5;
             longBreakMinutes = shortBreakMinutes * 3;
